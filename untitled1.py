e218e12794c49368b26737e7f1325d86dbaa5f56
maanasagu <128418746+maanasagu@users.noreply.github.com>
2023-05-05 09:31:44 -0400

    Percentage

--- untitled1.py
+++ untitled1.py
@@ -1,102 +1,67 @@
-import pandas
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 08:24:46 2023
 
-SubID = input('Enter Subject ID: ')
-conditions=pandas.read_csv("C:\\Users\\maana\\Documents\\GitHub\\TalkingHeads\\stim\\s_" + SubID + "\\" + SubID + "all_conditions.csv")
-conditions = conditions['0']
-right_answer= pandas.read_csv("C:\\Users\\maana\\Documents\\GitHub\\TalkingHeads\\stim\\s_" + SubID + "\\" + SubID + "Answerkey.csv")
-right_answer= right_answer.iloc[:,1:6].values.tolist()
-responses=pandas.read_csv("C:\\Users\\maana\\Documents\\GitHub\\TalkingHeads\\Output\\s_" + SubID + "\\" + SubID + "all_responses.csv")
-responses=responses.iloc[:,1:6].values.tolist()
+@author: maana
+"""
 
-overall_score = []
-mr_responses=[]
-mr_ak=[]
-mr_score=[]
-ml_responses=[]
-ml_ak=[]
-ml_score=[]
-mmr_responses=[]
-mmr_ak=[]
-mmr_score=[]
-mml_responses=[]
-mml_ak=[]
-mml_score=[]
+import pandas 
+import matplotlib.pyplot as plt
+df = pandas.read_excel("C:\\Users\\maana\\Documents\\GitHub\\TalkingHeads\\Pilot score comparison.xlsx", sheet_name='Sheet1')
+
+# Overall score plot
+
+x1 = df['subject Id']
+y1=df['overall score']
+# Create scatter plot
+plt.scatter(x1, y1)
+
+# Add labels and title
+plt.xlabel('Subject ID')
+plt.ylabel('Overall correct score (%)')
+plt.title('Overall score')
+
+# Show plot
+plt.show()
+
+# Matched vs Mismatched 
+
+y2=df['Matched_avg']
+y3=df['Mismatched_avg']
+plt.scatter(x1, y2,label='Matched')
+plt.scatter(x1, y3,label='Mismatched')
+# Add labels and title
+plt.xlabel('Subject ID')
+plt.ylabel('Correct score (%)')
+plt.title('Matched vs Mismatch')
+plt.legend()
+# Show plot
+plt.show()
 
 
-for i1 in range(len(conditions)):
-    
-    if conditions[i1]=='match right':
-        a=responses[i1]
-        b=right_answer[i1]
-        mr_responses.append(a)
-        mr_ak.append(b)
-        
-    elif conditions[i1]=='match left':
-        a=responses[i1]
-        b=right_answer[i1]
-        ml_responses.append(a)
-        ml_ak.append(b)
-        
-    elif conditions[i1]=='mismatch right':
-        a=responses[i1]
-        b=right_answer[i1]
-        mmr_responses.append(a)
-        mmr_ak.append(b)
-        
-    elif conditions[i1]=='mismatch left':
-        a=responses[i1]
-        b=right_answer[i1]
-        mml_responses.append(a)
-        mml_ak.append(b)
-  
-#Correct % in matched right
+# Right match vs Right mismatch
+y4=df['matched right']
+y5=df['mismatched right']
+plt.scatter(x1, y4,label='Matched')
+plt.scatter(x1, y5,label='Mismatched')
+# Add labels and title
+plt.xlabel('Subject ID')
+plt.ylabel('Correct score (%)')
+plt.title('Matched right vs Mismatch right')
+plt.legend()
+# Show plot
+plt.show()
 
-for i in range(len(mr_responses)):
-    row_correct=0
-    for j in range(len(mr_responses[i])):
-        if mr_responses[i][j]== mr_ak[i][j]:
-            row_correct+=1
-    mr_score.append(row_correct)    
-avgmr= sum(mr_score)/len(mr_score)    
-avgmr_percent= (avgmr/5)*100         
-      
-# Correct % in matched left
-for i in range(len(ml_responses)):
-    row_correct=0
-    for j in range(len(ml_responses[i])):
-        if ml_responses[i][j]== ml_ak[i][j]:
-            row_correct+=1
-    ml_score.append(row_correct)    
-avgml= sum(ml_score)/len(ml_score)    
-avgml_percent= (avgml/5)*100 
+# left match vs left mismatch
+y4=df['matched left']
+y5=df['mismatched left']
+plt.scatter(x1, y4,label='Matched')
+plt.scatter(x1, y5,label='Mismatched')
+# Add labels and title
+plt.xlabel('Subject ID')
+plt.ylabel('Correct score (%)')
+plt.title('Matched left vs Mismatch left')
+plt.legend()
+# Show plot
+plt.show()
 
-#Correct % in mismatched right
-for i in range(len(mmr_responses)):
-    row_correct=0
-    for j in range(len(mmr_responses[i])):
-        if mmr_responses[i][j]== mmr_ak[i][j]:
-            row_correct+=1
-    mmr_score.append(row_correct)    
-avgmmr= sum(mmr_score)/len(mmr_score)    
-avgmmr_percent= (avgmmr/5)*100 
-
-#Correct % in mismatched right
-for i in range(len(mml_responses)):
-    row_correct=0
-    for j in range(len(mml_responses[i])):
-        if mml_responses[i][j]== mml_ak[i][j]:
-            row_correct+=1
-    mml_score.append(row_correct)    
-avgmml= sum(mml_score)/len(mml_score)    
-avgmml_percent= (avgmml/5)*100 
-
-# Overall Correct %
-for i in range(len(right_answer)):
-    same_elements = 0
-    for j in range(len(right_answer[i])):
-        if right_answer[i][j] == responses[i][j]:
-            same_elements += 1
-    overall_score.append(same_elements)
-   
-avg= sum(overall_score)/len(overall_score)    
-avg_percent= (avg/5)*100 
---
+++ untitled2.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 09:29:03 2023
+
+@author: maana
+"""
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create a sample dataframe
+df = pd.DataFrame({
+    'subject Id': ['pilot002', '419_pilot2', '419_pilot3', '419_pilot4'],
+    'matched right': [93.33333333, 91.66666667, 78.33333333, 91.66666667],
+    'matched left': [90, 98.33333333, 93.33333333, 100],
+    'mismatched right': [56.66666667, 80, 66.66666667, 98.33333333],
+    'mismatched left': [88.33333333, 93.33333333, 80, 100]
+})
+
+# Set the subject Id column as the index
+df.set_index('subject Id', inplace=True)
+
+# Create a scatter plot for each subject Id
+for subject in df.index:
+    plt.scatter(df.columns, df.loc[subject], label=subject)
+    plt.plot(df.columns, df.loc[subject], label=subject)
+plt.xlabel('Test Conditions')
+plt.ylabel('Scores(%)')
+plt.title('Scores between subjects for Different test conditions')
+
+plt.show()
+
+df2= pd.DataFrame({
+    'subject Id': ['pilot002', '419_pilot2', '419_pilot3', '419_pilot4'],
+    'matched cases':[91.66666667,95,85.83333333,95.83333333],
+    'mismatched cases':[72.5,86.66666667,73.33333333,99.16666667]
+    })
+df2.set_index('subject Id', inplace=True)
+
+# Create a scatter plot for each subject Id
+for subject in df2.index:
+    plt.scatter(df2.columns, df2.loc[subject], label=subject)
+    plt.plot(df2.columns, df2.loc[subject], label=subject)
+plt.xlabel('Test Conditions')
+plt.ylabel('Scores(%)')
+plt.title('Scores between subjects for Different test conditions')
+plt.show()
