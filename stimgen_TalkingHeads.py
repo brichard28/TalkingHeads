374e83acc2fe438b1163ccd6e81aa2649a569b9b
maanasagu <128418746+maanasagu@users.noreply.github.com>
2023-05-12 12:10:23 -0400

    stimgen update

--- stimgen_TalkingHeads.py
+++ stimgen_TalkingHeads.py
@@ -211,59 +211,59 @@ for itrial in range(num_trials):
     if num==1:
         if condition_this_trial == 'match left' :
             audio1_spatialized = audio1_spatialized[1]
-            audio1_spatialized = audio1_spatialized["audio1_l"]
+            audio1_spatialized = audio1_spatialized["audio1_r"]
             rmsset = 0.02;
             rms = np.sqrt(np.mean(audio1_spatialized**2))
             audio1_spatialized = audio1_spatialized * rmsset/rms;
-            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_2_spatialized.wav", audio1_spatialized,fs_1)
+            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_1_spatialized.wav", audio1_spatialized,fs_1)
 
             audio2_spatialized = audio2_spatialized[1]
-            audio2_spatialized = audio2_spatialized["audio2_r"]
+            audio2_spatialized = audio2_spatialized["audio2_l"]
             rmsset = 0.02;
             rms = np.sqrt(np.mean(audio2_spatialized**2))
             audio2_spatialized = audio2_spatialized * rmsset/rms;
-            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_1_spatialized.wav",  audio2_spatialized, fs_2)
+            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_2_spatialized.wav",  audio2_spatialized, fs_2)
         elif  condition_this_trial == 'mismatch left':
                 audio1_spatialized = audio1_spatialized[1]
-                audio1_spatialized = audio1_spatialized["audio1_r"]
+                audio1_spatialized = audio1_spatialized["audio1_l"]
                 rmsset = 0.02;
                 rms = np.sqrt(np.mean(audio1_spatialized ** 2))
                 audio1_spatialized = audio1_spatialized * rmsset / rms;
-                sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_2_spatialized.wav", audio1_spatialized, fs_1)
+                sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_1_spatialized.wav", audio1_spatialized, fs_1)
 
                 audio2_spatialized = audio2_spatialized[1]
-                audio2_spatialized = audio2_spatialized["audio2_l"]
+                audio2_spatialized = audio2_spatialized["audio2_r"]
                 rmsset = 0.02;
                 rms = np.sqrt(np.mean(audio2_spatialized ** 2))
                 audio2_spatialized = audio2_spatialized * rmsset / rms;
-                sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_1_spatialized.wav", audio2_spatialized, fs_2)
+                sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_2_spatialized.wav", audio2_spatialized, fs_2)
 
         elif condition_this_trial == 'match right':
-            audio1_spatialized = audio1_spatialized[1]
-            audio1_spatialized = audio1_spatialized["audio1_l"]
-            rmsset = 0.02;
-            rms = np.sqrt(np.mean(audio1_spatialized**2))
-            audio1_spatialized = audio1_spatialized * rmsset/rms;
-            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_2_spatialized.wav", audio1_spatialized, fs_1)
-
-            audio2_spatialized = audio2_spatialized[1]
-            audio2_spatialized = audio2_spatialized["audio2_r"]
-            rms = np.sqrt(np.mean(audio2_spatialized**2))
-            audio2_spatialized = audio2_spatialized * rmsset/rms;
-            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_1_spatialized.wav",  audio2_spatialized, fs_1)
-        elif  condition_this_trial == 'mismatch right':
             audio1_spatialized = audio1_spatialized[1]
             audio1_spatialized = audio1_spatialized["audio1_r"]
             rmsset = 0.02;
             rms = np.sqrt(np.mean(audio1_spatialized**2))
             audio1_spatialized = audio1_spatialized * rmsset/rms;
-            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_2_spatialized.wav", audio1_spatialized, fs_1)
+            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_1_spatialized.wav", audio1_spatialized, fs_1)
 
             audio2_spatialized = audio2_spatialized[1]
             audio2_spatialized = audio2_spatialized["audio2_l"]
             rms = np.sqrt(np.mean(audio2_spatialized**2))
             audio2_spatialized = audio2_spatialized * rmsset/rms;
-            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_1_spatialized.wav",  audio2_spatialized, fs_1)
+            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_2_spatialized.wav",  audio2_spatialized, fs_1)
+        elif  condition_this_trial == 'mismatch right':
+            audio1_spatialized = audio1_spatialized[1]
+            audio1_spatialized = audio1_spatialized["audio1_l"]
+            rmsset = 0.02;
+            rms = np.sqrt(np.mean(audio1_spatialized**2))
+            audio1_spatialized = audio1_spatialized * rmsset/rms;
+            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_1_spatialized.wav", audio1_spatialized, fs_1)
+
+            audio2_spatialized = audio2_spatialized[1]
+            audio2_spatialized = audio2_spatialized["audio2_r"]
+            rms = np.sqrt(np.mean(audio2_spatialized**2))
+            audio2_spatialized = audio2_spatialized * rmsset/rms;
+            sf.write("D:\\Experiments\\TalkingHeads\\stim\\sentence_2_spatialized.wav",  audio2_spatialized, fs_1)
         
         
    
