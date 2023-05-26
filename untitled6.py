ca834021b524e3ee3cbd16bbe5bae1f267672717
maanasagu <128418746+maanasagu@users.noreply.github.com>
2023-05-26 13:57:13 -0400

    play tone

--- untitled6.py
+++ untitled6.py
@@ -5,11 +5,28 @@ Created on Wed May 24 10:22:58 2023
 @author: maana
 """
 
-import serial
+import numpy as np
+import sounddevice as sd
+import time
 
-ser = serial.Serial('COM4', 9600)
-ser.open()
+# Samples per second
+sps = 44100
 
-trigger_data = b'TRIGGER'
-ser.write(trigger_data)
-ser.close()   
+# Frequency / pitch
+freq_hz = 440.0
+
+# Duration
+duration_s = 5.0
+
+# Attenuation so the sound is reasonable
+atten = 0.3
+
+# NumpPy magic to calculate the waveform
+each_sample_number = np.arange(duration_s * sps)
+waveform = np.sin(2 * np.pi * each_sample_number * freq_hz / sps)
+waveform_quiet = waveform * atten
+
+# Play the waveform out the speakers
+sd.play(waveform_quiet, sps)
+time.sleep(duration_s)
+sd.stop()
