960ece6721694249b130e5774eb6d8827944310a
LiMN Lab Booth01 <limn@andrew.cmu.edu>
2023-05-30 09:30:48 -0400

    triggy testing

--- untitled6.py
+++ untitled6.py
@@ -28,10 +28,15 @@ waveform = np.sin(2 * np.pi * each_sample_number * freq_hz / sps)
 waveform_quiet = waveform * atten
 trigger_channel_3 = np.zeros(np.shape(waveform_quiet))
 trigger_channel_3[0] = 0.03
-waveform_quiet = np.transpose(np.stack(((waveform_quiet,waveform_quiet,trigger_channel_3,np.zeros(np.shape(waveform_quiet)),np.zeros(np.shape(waveform_quiet)),np.zeros(np.shape(waveform_quiet)),np.zeros(np.shape(waveform_quiet)),np.zeros(np.shape(waveform_quiet))))))
+#trigger_channel_3[452] = 0
+trigger_channel_4 = np.zeros(np.shape(waveform_quiet))
+trigger_channel_4[33000] = 0.03
+trigger_channel_5 = np.zeros(np.shape(waveform_quiet))
+trigger_channel_5[33000] = 0.03
+waveform_quiet = np.transpose(np.stack(((waveform_quiet,waveform_quiet,trigger_channel_3,trigger_channel_4,trigger_channel_5))))
 print(np.shape(waveform_quiet))
 # Play the waveform out the speakers
 sd.default.device = 'ASIO Fireface USB'
-sd.play(waveform_quiet, sps,mapping=[1,2,3,4,5,6,7,8])
+sd.play(waveform_quiet, sps,mapping=[1,2,3,4,5])
 time.sleep(duration_s)
 sd.stop()
