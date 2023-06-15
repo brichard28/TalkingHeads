749049135a32b48069e05beb99e0acf724445731
maanasagu <128418746+maanasagu@users.noreply.github.com>
2023-06-15 12:42:50 -0400

    arduino

---
+++ photoresistor/photoresistor.ino
@@ -0,0 +1,16 @@
+int photopin= A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int light= analogRead(photopin);
+  Serial.println(light);
+  delay(100);
+  
+
+}
