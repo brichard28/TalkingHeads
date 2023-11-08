2773e24aa8ff33d866e83b5e313d404bc07db07f
maanasagu <128418746+maanasagu@users.noreply.github.com>
2023-11-08 09:07:19 -0500

    pilot

--- untitled0.py
+++ untitled0.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 14 20:24:28 2023
+Created on Tue Oct 31 23:07:25 2023
 
 @author: maana
 """
-from pydub import AudioSegment
 
-start_time1 = 1970 
-end_time1 = 2837 
-audio_file1 = AudioSegment.from_file("C:/Users/maana/Documents/GitHub/TalkingHeads/stim/Structured Sentences F1_Mp3/Kathy gives nine old chairs_1.wav", format="wav")
-audio_frame1 = audio_file1[start_time1:end_time1]
-panned_audio_matchedleft1 = audio_frame1.pan(-1)
-panned_audio_mismatchleft1= audio_frame1.pan(1)
-panned_audio_matchedright1=audio_frame1.pan(1)
-panned_audio_mismatchright1=audio_frame1.pan(-1)
-panned_audio_matchedleft1.export("C:/Users/maana/Documents/GitHub/TalkingHeads/stim/Audio Cue/Chairs_F1_matchedleft.wav", format="wav")
-panned_audio_mismatchleft1.export("C:/Users/maana/Documents/GitHub/TalkingHeads/stim/Audio Cue/Chairs_F1_mismatchedleft.wav", format="wav")
-panned_audio_matchedright1.export("C:/Users/maana/Documents/GitHub/TalkingHeads/stim/Audio Cue/Chairs_F1_matchedright.wav", format="wav")
-panned_audio_mismatchright1.export("C:/Users/maana/Documents/GitHub/TalkingHeads/stim/Audio Cue/Chairs_F1_mismatchedright.wav", format="wav")
+import numpy as np
+# Define the AND gate truth table
+X = np.array([[-1,0, 0],
+              [-1,0, 1],
+              [-1,1, 0],
+              [-1,1, 1]])
+
+# Desired output for the AND gate
+y_true = np.array([0, 0, 0, 1])
+
+# Initialize weights and learning rate
+w = np.array([1, 2, 1])
+alpha = 1
+
+# Function to calculate the model prediction
+def predict(x, weights):
+    activation = np.dot(x, weights)
+    return 1 if activation > 0 else 0
+
